dc6d7670e89fb4e7b05ccca3a0bf700f1efe05a4
Hugo Lopes Tavares <hltbra@gmail.com>
2010-05-21 05:31:23 -0300

    improved long_description reading way and renamed package folder

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup, find_packages
 import sys, os
 
 version = '0.2.6'
+here = os.path.abspath(os.path.dirname(__file__))
+long_description = open(os.path.join(here, 'README.rst')).read()
 
 setup(name='specloud',
       version=version,
       description="install nosetests and plugins to ease bdd unit specs",
-      long_description=open('README.rst').read(),
+      long_description=long_description,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='test bdd nosetests spec unit',
       author='Hugo Lopes Tavares',
