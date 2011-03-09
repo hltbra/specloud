ad2a73e42b67c60b047e4219f76579d3e3136952
Hugo Lopes Tavares <hltbra@gmail.com>
2011-03-09 12:05:38 -0300

    added some trove classifiers and increased minor version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
-import sys, os
+import os
 
-version = '0.4.0'
+version = '0.4.1'
 here = os.path.abspath(os.path.dirname(__file__))
 long_description = open(os.path.join(here, 'README.rst')).read()
 
@@ -9,26 +9,34 @@ setup(name='specloud',
       version=version,
       description="install nosetests and plugins to ease bdd unit specs",
       long_description=long_description,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+      'Development Status :: 4 - Beta',
+      'Environment :: Console',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: MIT License',
+      'Natural Language :: English',
+      'Natural Language :: Portuguese (Brazilian)',
+      'Operating System :: OS Independent',
+      'Topic :: Software Development :: Testing',
+      'Topic :: Utilities',
+      ],
       keywords='test bdd nosetests spec unit',
       author='Hugo Lopes Tavares',
       author_email='hltbra@gmail.com',
       url='http://github.com/hugobr/specloud',
       license='MIT',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
           'nose',
           'figleaf',
           'pinocchio',
-          # -*- Extra requirements: -*-
       ],
       dependency_links = [
           'http://darcs.idyll.org/~t/projects/pinocchio-latest.tar.gz#egg=pinocchio-dev',
       ],
       entry_points="""
-      # -*- Entry points: -*-
       [console_scripts]
       specloud = specloud:main
       """,
