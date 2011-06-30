6b0f2b140dcdcf6aba8388a921bd4c5c95590344
Hugo Lopes Tavares <hltbra@gmail.com>
2011-06-30 11:17:06 -0300

    bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.4.3'
+version = '0.4.4'
 here = os.path.abspath(os.path.dirname(__file__))
 long_description = open(os.path.join(here, 'README.rst')).read()
 
