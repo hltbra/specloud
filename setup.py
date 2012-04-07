428c2c582131151359c3bcebdbb625338b60a21f
Hugo Lopes Tavares <hltbra@gmail.com>
2012-04-06 22:12:46 -0300

    Bump version to 0.4.5 due to pinocchio version update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.4.4'
+version = '0.4.5'
 here = os.path.abspath(os.path.dirname(__file__))
 long_description = open(os.path.join(here, 'README.rst')).read()
 
